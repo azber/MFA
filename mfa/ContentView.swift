2560d1cb983a09a677cc30ee53473984e47b6fb3
azberx <azberx@gmail.com>
2025-03-31 11:32:12 +0800

    Initial Commit

---
+++ mfa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mfa
+//
+//  Created by azberx on 2025/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mfa/mfaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mfaApp.swift
+//  mfa
+//
+//  Created by azberx on 2025/3/31.
+//
+
+import SwiftUI
+
+@main
+struct mfaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
